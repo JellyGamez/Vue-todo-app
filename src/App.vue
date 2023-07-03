2407439163c692fa875c977ad7d697d9d81e0551
JellyGamez <75379898+JellyGamez@users.noreply.github.com>
2023-07-03 13:07:26 +0300

    improved readability

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <script setup>
 import { computed, ref, provide } from 'vue';
-import TaskList from './components/TaskList.vue'
+import TaskList from './components/TaskList.vue';
+import ConfirmEditIcon from './components/icons/ConfirmEditIcon.vue';
+import AddIcon from './components/icons/AddIcon.vue';
 
 const tasks = ref([
     { label: "Do something", completed: false, id: 1      },
     { label: "Do something else", completed: false, id: 2 },
     { label: "Do this instead", completed: false, id: 3   },
+    { label: "Do this insteaddd", completed: false, id: 4 },
 ]);
-const input = ref();
+const input = ref('');
+const searchInput = ref('');
 const idToEdit = ref();
-
 
 const isEditing = computed(() => idToEdit.value !== undefined);
 const filters = computed(() => {
   return {
-        todo: tasks.value.filter(task => !task.completed),
-        completed: tasks.value.filter(task => task.completed),
-      }
+    todo: tasks.value.filter(task => !task.completed && task.label.includes(searchInput.value.trim())),
+    completed: tasks.value.filter(task => task.completed && task.label.includes(searchInput.value.trim())),
+  }
 })
 
 function addNewTask() {
@@ -26,14 +29,11 @@ function addNewTask() {
       task.label = input.value;
       idToEdit.value = undefined;
     }
-  else
-    tasks.value.push({label: input.value, completed: false, id: tasks.value.length + 1});
+  else if (input.value.trim() !== '')
+    tasks.value.push({label: input.value.trim(), completed: false, id: tasks.value.length + 1});
   input.value = '';
 }
 
-function completeTask(taskId) {
-  tasks.value.find(task => task.id)
-}
 function deleteTask(taskId) {
   tasks.value = tasks.value.filter(task => task.id !== taskId);
 }
@@ -48,24 +48,32 @@ provide('editingTask', editingTask);
 </script>
 
 <template>
-  <div class="flex flex-col gap-6 bg-slate-100 border rounded-lg p-4">
+  <div class="flex flex-col gap-6 bg-gray-600 rounded-xl p-4">
 
-    <div class="flex border rounded">
-      <input class="w-full px-2" type="text" :placeholder="!isEditing ? 'Add new task' : 'Edit task'" v-model="input">
-      <button class="bg-white py-2 px-4 border" @click="addNewTask"> 
+    <div class="flex">
+      <input class="w-full px-2 border-b border-white mr-4 bg-gray-600 text-white outline-none" type="text" :placeholder="!isEditing ? 'Add new task' : 'Edit task'" v-model="input">
+      <button class="py-2 px-2 rounded-xl bg-gray-500" @click="addNewTask"> 
         
         <template v-if="isEditing">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-          <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-          </svg>
+          <ConfirmEditIcon/>
         </template>
+
         <template v-else="">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-          <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-          </svg>
+          <AddIcon  />
         </template>
       
       </button>
+    </div>
+
+    <div class="flex">
+      <input class="w-full px-2 border-b border-white mr-4 bg-gray-600 text-white outline-none" type="text" placeholder="Search task" v-model="searchInput">
+      <div class="py-2 px-2 bg-gray-500 rounded-xl"> 
+        
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-5 h-5">
+        <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+        </svg>
+
+      </div>
     </div>
     
     <TaskList listLabel="To do tasks" :tasks="filters.todo"/>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -7,7 +7,7 @@ defineProps({ listLabel: String, tasks: Array})
 
 <template>
     <div v-if="tasks.length">
-        <h1 class="font-bold text-2xl text-center mb-5"> {{ listLabel }} </h1>
+        <h1 v-if="tasks.length" class="font-bold text-2xl text-white text-center mb-5"> {{ listLabel }} </h1>
         <ul class="flex flex-col gap-5">
             <li v-for="task in tasks" :key="task.id">
                 <TaskItem :task="task"/>
