f76cc393515def90b0f2b76063825b4cf863329c
JellyGamez <75379898+JellyGamez@users.noreply.github.com>
2023-08-12 14:38:34 +0300

    improved search

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import ConfirmEditIcon from './components/icons/ConfirmEditIcon.vue';
 import AddIcon from './components/icons/AddIcon.vue';
 import SearchIcon from './components/icons/SearchIcon.vue';
 const tasks = ref([
-    { label: "Throw out the trash", completed: false, id: 1      },
-    { label: "Finish the homework", completed: false, id: 2 },
-    { label: "Water the flowers", completed: false, id: 3   },
-    { label: "Buy groceries", completed: false, id: 4 },
+  { label: "Throw out the trash", completed: false, id: 1 },
+  { label: "Finish the homework", completed: false, id: 2 },
+  { label: "Water the flowers", completed: false, id: 3 },
+  { label: "Buy groceries", completed: false, id: 4 },
 ]);
 const mainInput = ref('');
 const searchInput = ref('');
@@ -17,21 +17,20 @@ const idToEdit = ref();
 const isEditing = computed(() => idToEdit.value !== undefined);
 const filters = computed(() => {
   return {
-    todo: tasks.value.filter(task => !task.completed && task.label.includes(searchInput.value.trim())),
-    completed: tasks.value.filter(task => task.completed && task.label.includes(searchInput.value.trim())),
+    todo: tasks.value.filter(task => !task.completed && task.label.toLowerCase().includes(searchInput.value.trim().toLowerCase())),
+    completed: tasks.value.filter(task => task.completed && task.label.toLowerCase().includes(searchInput.value.trim().toLowerCase())),
   }
 });
 
 function addNewTask() {
-  if (isEditing.value)
-    {
-      const task = tasks.value.find(task => task.id === idToEdit.value);
-      task.label = mainInput.value;
-      idToEdit.value = undefined;
-    }
+  if (isEditing.value) {
+    const task = tasks.value.find(task => task.id === idToEdit.value);
+    task.label = mainInput.value;
+    idToEdit.value = undefined;
+  }
   else if (mainInput.value.trim() !== '')
-    tasks.value.push({label: mainInput.value.trim(), completed: false, id: tasks.value.length + 1});
-    mainInput.value = '';
+    tasks.value.push({ label: mainInput.value.trim(), completed: false, id: tasks.value.length + 1 });
+  mainInput.value = '';
 };
 function deleteTask(taskId) {
   tasks.value = tasks.value.filter(task => task.id !== taskId);
@@ -47,41 +46,36 @@ provide('editingTask', editingTask);
 </script>
 
 <template>
-  <div class="flex flex-col flex-grow overflow-y-auto gap-6 bg-green-600 rounded-xl p-6 absolute top-24 w-1/3 justify-center">
+  <div
+    class="flex flex-col flex-grow overflow-y-auto gap-6 bg-green-600 rounded-xl p-6 absolute top-24 w-1/3 justify-center">
 
     <div class="flex">
-      <input class="w-full px-2 border-b border-white bg-green-600 mr-4 text-white placeholder-white outline-none" 
-            type="text" 
-            :placeholder="!isEditing ? 'Add new task' : 'Edit task'" 
-            v-model="mainInput"
-      >
-      <button class="py-2 px-2 rounded-full bg-green-700" @click="addNewTask"> 
-        
+      <input class="w-full px-2 border-b border-white bg-green-600 mr-4 text-white placeholder-white outline-none"
+        type="text" :placeholder="!isEditing ? 'Add new task' : 'Edit task'" v-model="mainInput">
+      <button class="py-2 px-2 rounded-full bg-green-700" @click="addNewTask">
+
         <template v-if="isEditing">
-          <ConfirmEditIcon/>
+          <ConfirmEditIcon />
         </template>
 
         <template v-else="">
-          <AddIcon/>
-        </template>   
-      
+          <AddIcon />
+        </template>
+
       </button>
     </div>
 
     <div class="flex">
-      <input class="w-full px-2 border-b border-white bg-green-600 mr-4 text-white placeholder-white outline-none" 
-            type="text" 
-            placeholder="Search task" 
-            v-model="searchInput"
-      >
-      <div class="py-2 px-2 bg-green-700 rounded-full"> 
-        <SearchIcon/>
+      <input class="w-full px-2 border-b border-white bg-green-600 mr-4 text-white placeholder-white outline-none"
+        type="text" placeholder="Search task" v-model="searchInput">
+      <div class="py-2 px-2 bg-green-700 rounded-full">
+        <SearchIcon />
       </div>
     </div>
-    
-    <TaskList listLabel="To do tasks" :tasks="filters.todo"/>
 
-    <TaskList listLabel="Completed tasks" :tasks="filters.completed"/>
+    <TaskList listLabel="To do tasks" :tasks="filters.todo" />
+
+    <TaskList listLabel="Completed tasks" :tasks="filters.completed" />
 
   </div>
 </template>
